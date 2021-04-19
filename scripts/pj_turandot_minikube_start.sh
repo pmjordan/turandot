3d8dd03259add742648847e81081a7bb0a8f6abc
Paul Jordan <paul.m.jordan@bt.com>
2021-04-19 09:04:54 +0100

    verify resposure during minikube start

--- scripts/pj_turandot_minikube_start.sh
+++ scripts/pj_turandot_minikube_start.sh
@@ -4,6 +4,6 @@ kubectl config set-context --current --namespace=workspace
 turandot operator install --site=central --role=view --wait -v
 reposure registry create default --provider=minikube --wait -v
 minikube status
-
+reposure registry list
 
 
