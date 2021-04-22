d2c24d9d688f8638d8439e758f89dbbd8f3cc72b
Paul Jordan <paul.m.jordan@bt.com>
2021-04-22 11:32:10 +0100

    remove redundant namespace create

--- scripts/pj_turandot_minikube_start.sh
+++ scripts/pj_turandot_minikube_start.sh
@@ -1,5 +1,4 @@
 minikube start --addons=registry ...
-kubectl create namespace workspace
 kubectl config set-context --current --namespace=workspace
 turandot operator install --site=central --role=view --wait -v
 reposure registry create default --provider=minikube --wait -v
