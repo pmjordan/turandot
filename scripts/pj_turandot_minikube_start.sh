5de7b3cb48b21fe78551d580a92d43f9c3b926a9
Paul Jordan <paul.m.jordan@bt.com>
2021-05-15 20:06:32 +0100

    tosca deploys component-controller

--- scripts/pj_turandot_minikube_start.sh
+++ scripts/pj_turandot_minikube_start.sh
@@ -1,6 +1,6 @@
 minikube start --addons=registry ...
 kubectl config set-context --current --namespace=workspace
-turandot operator install --role=view --site=central --wait -v
+turandot operator install --role=view --site=central --wait -vv --log log.txt
 reposure operator install --role=view --wait -v
 reposure registry create default --provider=minikube --wait -v
 kubectl apply --filename=https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
