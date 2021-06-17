9f2c34ae0c15b9ea19764e2fbfcd52c6d0265990
Paul Jordan <paul.m.jordan@bt.com>
2021-06-17 09:53:00 +0100

    pre-load cert-manager

--- scripts/pj_turandot_minikube_start.sh
+++ scripts/pj_turandot_minikube_start.sh
@@ -1,11 +1,17 @@
-# ingress addon is required by canvas operatpr, quicker to start it here
+# ingress addon is required by canvas operator, quicker to start it here
 minikube start --addons registry ingress 
 #minikube start --addons=registry ...
+#install canvas
+cd /opt/oda-canvas-charts
+kubectl create namespace canvas
+./install_canvas.sh
+cd ~
+#install turandot
 kubectl config set-context --current --namespace=workspace
 turandot operator install --role=view --site=central --wait -vv
 reposure operator install --role=view --wait -v
 reposure registry create default --provider=minikube --wait -v
-kubectl apply --filename=https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
+kubectl apply --filename=assets/kubernetes/cert-manager.yaml
 reposure simple install --authentication --wait -v
 reposure registry create default --provider=simple --wait -v
 minikube status
