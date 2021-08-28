e7267d581788421d854473fb7012dda18f24af67
Paul Jordan <paul.m.jordan@bt.com>
2021-08-28 20:02:51 +0100

    install minikube my way

--- lab/pj_install_deb.sh
+++ lab/pj_install_deb.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # installs packages to ubuntu like my local machine. Consider also changing equivalent file used for azure centos.
+
+
+
 # install helm
 cd /tmp
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
@@ -26,7 +29,14 @@ wget -O puccini.deb https://github.com/tliron/puccini/releases/download/v0.18.0/
 sudo apt install ./puccini.deb
 
 #Install kubectl and minikube here as can't get them from cloud-init packages
-/opt/turandot/lab/install
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install minikube 
+cd /tmp
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
 
 sudo wall -n "Completed turandot tools installation. Start a new session to use new permissions and cd to /opt/turandot"
 
